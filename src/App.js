82eac570081d3939ea6fda1b8ae982b9c5f19c97
Emily Ingraham <emilyvingraham@gmail.com>
2024-02-25 14:01:24 -0700

    Added basic css styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
 import Weather from './Weather.js';
 import Footer from './Footer.js';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 export default function App() {
--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,32 @@
-import './Weather.css';
+import React from 'react';
+import './App.css';
 import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function Weather() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-4">
+                <div className="col-md-8">
                     <input
                         className="form-control"
                         type="text"
                         placeholder="Enter a city"
                     />
                 </div>
-                <div className="col-md-4">
-                    <button className="btn btn-primary">Search</button>
+                <div className="col-md-2">
+                    <button className="btn btn-primary w-100">Search</button>
                 </div>
-                <div className="col-md-4">
-                    <button className="btn  btn-success">Current</button>
+                <div className="col-md-2">
+                    <button className="btn  btn-success w-100">Local</button>
                 </div>
             </div>
-            <div className="row">
+            <div className="row py-3">
                 <div className="col-md-6">
-                    <h2>Grand Junction</h2>
-                    <p>Sunny</p>
+                    <h2 className="pb-0 mb-0">Grand Junction</h2>
+                    <p className="my-0">Sunday 15:00</p>
+                    <p className="my-0">Sunny</p>
                 </div>
+                <div className="col-md-3" />
                 <div className="col-md-3">
                     <ReactAnimatedWeather
                         icon="CLEAR_DAY"
@@ -35,16 +38,20 @@ export default function Weather() {
             </div>
             <div className="row">
                 <div className="col-md-4">
-                    <h2>54°F | °C</h2>
+                    <strong>
+                        <p className="fs-1">
+                            54<sup className="fs-4">°F | °C</sup>
+                        </p>
+                    </strong>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-4 lh-1">
                     <ul>
                         <li>Colorado, USA</li>
                         <li>Longetitude</li>
                         <li>Latitude</li>
                     </ul>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-4 lh-1">
                     <ul>
                         <li>Preciptation: 0%</li>
                         <li>Humidity: 0%</li>
