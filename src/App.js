767d7802a4ee34317f31828ee29ce0f48f15e1a5
Emily Ingraham <emilyvingraham@gmail.com>
2024-02-25 11:24:26 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 export default function App() {
     return (
         <div className="App">
-            <h1>React Weather Application</h1>
-            This project was built by{' '}
-            <a
-                href="https://github.com/emilyvingraham"
-                target="_blank"
-                rel="noreferrer"
-            >
-                Emily Ingraham
-            </a>{' '}
-            and is{' '}
-            <a
-                href="https://github.com/emilyvingraham/react-weather-app"
-                target="_blank"
-                rel="noreferrer"
-            >
-                open-sourced on Github
-            </a>
-            .
+            <div className="container">
+                <h1>React Weather Application</h1>
+                This project was built by{' '}
+                <a
+                    href="https://github.com/emilyvingraham"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Emily Ingraham
+                </a>{' '}
+                and is{' '}
+                <a
+                    href="https://github.com/emilyvingraham/react-weather-app"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    open-sourced on Github
+                </a>
+                .
+            </div>
         </div>
     );
 }
