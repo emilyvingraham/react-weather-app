72556498b0806d21ffa80ff835d0697a31aa3347
Emily Ingraham <emilyvingraham@gmail.com>
2024-02-25 15:22:10 -0700

    Updated styling for responsiveness and imported axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function Weather() {
+    let apiKey = '8c78e9e7e9928cd1a2a6f923072c3dec';
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`;
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-8">
                     <input
-                        className="form-control"
+                        className="form-control SearchResponsive"
                         type="text"
                         placeholder="Enter a city"
                     />
                 </div>
                 <div className="col-md-2">
-                    <button className="btn btn-primary w-100">Search</button>
+                    <button className="btn btn-primary w-100 SearchResponsive">
+                        Search
+                    </button>
                 </div>
                 <div className="col-md-2">
                     <button className="btn  btn-success w-100">Local</button>
                 </div>
             </div>
-            <div className="row py-3">
+            <div className="row py-3 TextResponsive">
                 <div className="col-md-6">
-                    <h2 className="pb-0 mb-0">Grand Junction</h2>
-                    <p className="my-0">Sunday 15:00</p>
-                    <p className="my-0">Sunny</p>
+                    <h2 className="pb-0 mb-0">
+                        <strong>Grand Junction</strong>
+                    </h2>
+                    <p>Sunny</p>
                 </div>
-                <div className="col-md-3" />
+                <div className="col-md-3">
+                    <p className="fs-1">
+                        <strong>54</strong>
+                        <sup className="fs-6">
+                            <strong>°F | °C</strong>
+                        </sup>
+                    </p>
+                </div>
                 <div className="col-md-3">
                     <ReactAnimatedWeather
                         icon="CLEAR_DAY"
                         color="BLACK"
-                        size={100}
+                        size={75}
                         animate={true}
                     />
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-4">
-                    <strong>
-                        <p className="fs-1">
-                            54<sup className="fs-4">°F | °C</sup>
-                        </p>
-                    </strong>
-                </div>
+            <div className="row TextResponsive">
+                <div className="col-md-2" />
                 <div className="col-md-4 lh-1">
                     <ul>
                         <li>Colorado, USA</li>
@@ -58,6 +66,7 @@ export default function Weather() {
                         <li>Wind: 5mph</li>
                     </ul>
                 </div>
+                <div className="col-md-2" />
             </div>
         </div>
     );
